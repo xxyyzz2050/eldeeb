79b0ceaabb6f8653431785affd5f2ffbb6d2319b
admin <admin@almogtama3.com>
2019-04-17 08:40:51 +0300

    remove custom/ as it not part of the library

--- src/lib/custom/index.ts
+++
@@ -1,9 +0,0 @@
-import eldeeb from '../index.js'
-//nx: extend lib/index.js
-
-eldeeb.data = function(root) {
-  let data = require('./data.js').default
-  return new data(root)
-}
-
-export default eldeeb //or: merge(eldeeb,overrides)
